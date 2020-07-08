14e9a0448ecec28d96808c387bbd2c0004490908
Jeff-Adler <Jeffm.adler@gmail.com>
2020-07-08 15:09:28 -0400

    added genres to artists

--- db/migrate/2020070711943_create_artists.rb
+++ db/migrate/2020070711943_create_artists.rb
@@ -3,6 +3,7 @@ class CreateArtists < ActiveRecord::Migration[6.0]
         create_table :artists do |t|
             t.string :name
             t.integer :popularity
+            t.string :genres
         end
     end
 end
