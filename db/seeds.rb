c22643e0d542e6edb2ce665c2b2ad6a645cb3095
Luis Reyes <luisreyesxv@yahoo.com>
2020-07-11 17:26:07 -0400

    reformating seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,145 +20,211 @@ def add_artistgenre_seed(artist_name)
     new_artist 
 end
 
-#profile: alt-rock fan
-a11 = add_artistgenre_seed("Guided By Voices")
-a12 = add_artistgenre_seed("Tame Impala")
-a13 = add_artistgenre_seed("Pavement")
-a14 = add_artistgenre_seed("Silver Jews")
-a15  = add_artistgenre_seed("Nirvana")
-a16 = add_artistgenre_seed("Stereolab")
-a17  = add_artistgenre_seed("Dope Smoker")
-a18  = add_artistgenre_seed("Boris")
-a19 = add_artistgenre_seed("The Minutemen")
-a110 = add_artistgenre_seed("The Smashing Pumpkins")
-alt_rock_u1 = User.create(name:"Krystal", age: 23, city: "NYC", email: "krystalemail")
-UserArtist.create(user: alt_rock_u1, artist: a11)
-UserArtist.create(user: alt_rock_u1, artist: a12)
-UserArtist.create(user: alt_rock_u1, artist: a13)
-UserArtist.create(user: alt_rock_u1, artist: a14)
-UserArtist.create(user: alt_rock_u1, artist: a15)
-UserArtist.create(user: alt_rock_u1, artist: a16)
-UserArtist.create(user: alt_rock_u1, artist: a17)
-UserArtist.create(user: alt_rock_u1, artist: a18)
-UserArtist.create(user: alt_rock_u1, artist: a19)
-UserArtist.create(user: alt_rock_u1, artist: a110)
 
-#profile: hip-hop fan
-a21 = add_artistgenre_seed("Kanye West")
-a22 = add_artistgenre_seed("JAY-Z")
-a23 = add_artistgenre_seed("Chance The Rapper")
-a24  = add_artistgenre_seed("MF DOOM")
-a25  = add_artistgenre_seed("Rihanna")
-a26  = add_artistgenre_seed("Tupac")
-a27  = add_artistgenre_seed("Notorious B.I.G")
-a28  = add_artistgenre_seed("The Roots")
-a29  = add_artistgenre_seed("Gangstarr")
-a210 = add_artistgenre_seed("Childish Gambino")
-hip_hop_u1 = User.create(name:"bill", age: 23, city: "NYC", email: "billemail")
-UserArtist.create(user: hip_hop_u1, artist: a21)
-UserArtist.create(user: hip_hop_u1, artist: a22)
-UserArtist.create(user: hip_hop_u1, artist: a23)
-UserArtist.create(user: hip_hop_u1, artist: a24)
-UserArtist.create(user: hip_hop_u1, artist: a25)
-UserArtist.create(user: hip_hop_u1, artist: a26)
-UserArtist.create(user: hip_hop_u1, artist: a27)
-UserArtist.create(user: hip_hop_u1, artist: a28)
-UserArtist.create(user: hip_hop_u1, artist: a29)
-UserArtist.create(user: hip_hop_u1, artist: a210)
+def create_profile(song_array,user_details)
+    user = User.create(name: user_details[0], age: user_details[1], city: user_details[2], email: user_details[3])
 
-#profile: pop fan
-a31 = add_artistgenre_seed("Kanye West")
-a32 = add_artistgenre_seed("Rihanna")
-a33 = add_artistgenre_seed("Tame Impala")
-a34 = add_artistgenre_seed("Billie Eilish")
-a35  = add_artistgenre_seed("Justin Bieber")
-a36 = add_artistgenre_seed("Maroon 5")
-a37  = add_artistgenre_seed("Ed Sheeran")
-a38  = add_artistgenre_seed("Michael Jackson")
-a39 = add_artistgenre_seed("Sean Paul")
-a310 = add_artistgenre_seed("Eminem")
-pop_u1 = User.create(name:"cameron", age: 43, city: "NYC", email: "cameronemail")
-UserArtist.create(user: pop_u1, artist: a31)
-UserArtist.create(user: pop_u1, artist: a32)
-UserArtist.create(user: pop_u1, artist: a33)
-UserArtist.create(user: pop_u1, artist: a34)
-UserArtist.create(user: pop_u1, artist: a35)
-UserArtist.create(user: pop_u1, artist: a36)
-UserArtist.create(user: pop_u1, artist: a37)
-UserArtist.create(user: pop_u1, artist: a38)
-UserArtist.create(user: pop_u1, artist: a39)
-UserArtist.create(user: pop_u1, artist: a310)
+    artist_array.each do |artist_name|
+        artist = add_artistgenre_seed(artist_name)
+        UserArtist.create(user: user, artist: artist)
+    end
+    puts "#{user_details[0]} done"
+end
 
-#profile: grunge fan
-a41 = add_artistgenre_seed("Nirvana")
-a42 = add_artistgenre_seed("Pearl Jam")
-a43 = add_artistgenre_seed("Alice in Chains")
-a44 = add_artistgenre_seed("The Smashing Pumpkins")
-a45  = add_artistgenre_seed("Pavement")
-a46 = add_artistgenre_seed("Soundgarden")
-a47  = add_artistgenre_seed("Guided By Voices")
-a48  = add_artistgenre_seed("Stone Temple Pilots")
-a49 = add_artistgenre_seed("Korn")
-a410 = add_artistgenre_seed("Limp Bizkit")
-grunge_u1 = User.create(name:"vandad", age: 37, city: "Brooklyn", email: "vandademail")
-UserArtist.create(user: grunge_u1, artist: a41)
-UserArtist.create(user: grunge_u1, artist: a42)
-UserArtist.create(user: grunge_u1, artist: a43)
-UserArtist.create(user: grunge_u1, artist: a44)
-UserArtist.create(user: grunge_u1, artist: a45)
-UserArtist.create(user: grunge_u1, artist: a46)
-UserArtist.create(user: grunge_u1, artist: a47)
-UserArtist.create(user: grunge_u1, artist: a48)
-UserArtist.create(user: grunge_u1, artist: a49)
-UserArtist.create(user: grunge_u1, artist: a410)
 
-#profile: electronic fan
-a51 = add_artistgenre_seed("Aphex Twin")
-a52 = add_artistgenre_seed("BTS")
-a53 = add_artistgenre_seed("Skrillex")
-a54 = add_artistgenre_seed("Tygo")
-a55  = add_artistgenre_seed("Oneohtrix Point Never")
-a56 = add_artistgenre_seed("Ryuichi Sakamoto")
-a57  = add_artistgenre_seed("Yellow Magic Orchestra")
-a58  = add_artistgenre_seed("Kanye West")
-a59 = add_artistgenre_seed("MF DOOM")
-a510 = add_artistgenre_seed("J Dilla")
-electronic_u1 = User.create(name:"leslie", age: 23, city: "NYC", email: "leslieemail")
-UserArtist.create(user: electronic_u1, artist: a51)
-UserArtist.create(user: electronic_u1, artist: a52)
-UserArtist.create(user: electronic_u1, artist: a53)
-UserArtist.create(user: electronic_u1, artist: a54)
-UserArtist.create(user: electronic_u1, artist: a55)
-UserArtist.create(user: electronic_u1, artist: a56)
-UserArtist.create(user: electronic_u1, artist: a57)
-UserArtist.create(user: electronic_u1, artist: a58)
-UserArtist.create(user: electronic_u1, artist: a59)
-UserArtist.create(user: electronic_u1, artist: a510)
+#profile: user 1
+user= ["Monique Chance", 30, "Jersey","monique@totallyfake.com"]
+songs = ["Kanye West","J Cole","Chris Brown","Drake","Nicki Minaj","Jay-z","Summer Walker", "Jhene Aiko", "Dababy","Big Sean"]
+create_profile(songs,user)
 
-#profile: hiphop/pop fans
-hip_hop_pop_u1 = User.create(name: "chrys", age: 97, city: "NJ", email: "chrysemail")
-UserArtist.create(user: hip_hop_pop_u1, artist: a21)
-UserArtist.create(user: hip_hop_pop_u1, artist: a22)
-UserArtist.create(user: hip_hop_pop_u1, artist: a23)
-UserArtist.create(user: hip_hop_pop_u1, artist: a24)
-UserArtist.create(user: hip_hop_pop_u1, artist: a25)
-UserArtist.create(user: hip_hop_pop_u1, artist: a36)
-UserArtist.create(user: hip_hop_pop_u1, artist: a37)
-UserArtist.create(user: hip_hop_pop_u1, artist: a38)
-UserArtist.create(user: hip_hop_pop_u1, artist: a39)
-UserArtist.create(user: hip_hop_pop_u1, artist: a310)
 
-hip_hop_pop_u2 = User.create(name: "jess", age: 92, city: "Connecticut", email: "jessemail")
-UserArtist.create(user: hip_hop_pop_u2, artist: a31)
-UserArtist.create(user: hip_hop_pop_u2, artist: a32)
-UserArtist.create(user: hip_hop_pop_u2, artist: a33)
-UserArtist.create(user: hip_hop_pop_u2, artist: a34)
-UserArtist.create(user: hip_hop_pop_u2, artist: a35)
-UserArtist.create(user: hip_hop_pop_u2, artist: a26)
-UserArtist.create(user: hip_hop_pop_u2, artist: a27)
-UserArtist.create(user: hip_hop_pop_u2, artist: a28)
-UserArtist.create(user: hip_hop_pop_u2, artist: a29)
-UserArtist.create(user: hip_hop_pop_u2, artist: a210)
+
+#profile: user 2
+user= ["Bridget Brito", 24, "Bronx","bridget@totallyfake.com"]
+songs = ["Bad Bunny","NCT 127","hasley","j cole","EXO","Childish Gambino","Billie Eilish","Fleetwood Mac","Jessie Reyez","6lack"]
+create_profile(songs,user)
+
+#profile : user 3
+user= ["Sharina Aybar", 25, "Bronx","sharina@totallyfake.com"]
+songs = ["Circa Survive","j cole", "Mac Miller", "Earthgang", "eli sostre", "bryson tiller", "etta james","billie holiday",'kimbra',"gary moore"]
+create_profile(songs,user)
+
+#profile : user 4
+user= ["Crystal Santiago", 26, "Bronx","crystal@totallyfake.com"]
+songs = ["george ezra", "vance joy","bad bunny","big wild","summer walker","hobo johnson","bazzi","post malone","travis scott",'ozuna']
+create_profile(songs,user)
+
+#profile : user 5
+user= ["Kate McDonnell", 26, "Ireland","kate@totallyfake.com"]
+songs = ["the 1975","cardi b","dua lipa","artic monkeys","gun n roses","bruno mars","beyonce","fleetwood mac","lady gaga","Michael jackson"]
+create_profile(songs,user)
+
+#profile: user 6
+user= ["Seongkon Kang", 30, "Flushing","seong@totallyfake.com"]
+songs = ["slowthai","grace craters","lil simz","dermot kennedy","lewis capaldi","ry x","mf doom","four tet","dj okawari","run the jewels"]
+create_profile(songs,user)
+
+#profile: user 7
+user= ["Edwin Casado", 28, "West New York","edwin@totallyfake.com"]
+songs = ["heroes del silencio","the smashing pumpkins","toque profundo",'tabutek',"iron maiden","coheed and cambria","radiohead","soda stereo","la ley","the shins"]
+create_profile(songs,user)
+
+#profile: user 8
+user= ["Andrew", 30, "Chicago","andrew@totallyfake.com"]
+songs = ["Dropkick Murphys","the fratellis","kanye","queen","Coldplay","led zepplin","the black keys","muse","the 1975","red hot chili peppers"]
+create_profile(songs,user)
+
+#profile: user 9
+user= ["Steve", 33, "Metro North","steve@totallyfake.com"]
+songs = ["Streetlight Manifesto","The Offspring","Less than Jake","Sublime","No Doubt","weezer","Catch 22","nirvana","dead kennedys","coheed and cambria"]
+create_profile(songs,user)
+
+#profile: user 10
+user= ["Cool Mike", 69, "Coolsville","coolmike@totallyfake.com"]
+songs = ["Boogaloo Assassins","The Cat Empire","celia cruz","bruno mars","southern culture on the skids","metallica","Waka flocka flame","the hives","fulanito","nujabes"]
+create_profile(songs,user)
+
+
+
+# #profile: alt-rock fan
+# a11 = add_artistgenre_seed("Guided By Voices")
+# a12 = add_artistgenre_seed("Tame Impala")
+# a13 = add_artistgenre_seed("Pavement")
+# a14 = add_artistgenre_seed("Silver Jews")
+# a15  = add_artistgenre_seed("Nirvana")
+# a16 = add_artistgenre_seed("Stereolab")
+# a17  = add_artistgenre_seed("Dope Smoker")
+# a18  = add_artistgenre_seed("Boris")
+# a19 = add_artistgenre_seed("The Minutemen")
+# a110 = add_artistgenre_seed("The Smashing Pumpkins")
+# alt_rock_u1 = User.create(name:"Krystal", age: 23, city: "NYC", email: "krystalemail")
+# UserArtist.create(user: alt_rock_u1, artist: a11)
+# UserArtist.create(user: alt_rock_u1, artist: a12)
+# UserArtist.create(user: alt_rock_u1, artist: a13)
+# UserArtist.create(user: alt_rock_u1, artist: a14)
+# UserArtist.create(user: alt_rock_u1, artist: a15)
+# UserArtist.create(user: alt_rock_u1, artist: a16)
+# UserArtist.create(user: alt_rock_u1, artist: a17)
+# UserArtist.create(user: alt_rock_u1, artist: a18)
+# UserArtist.create(user: alt_rock_u1, artist: a19)
+# UserArtist.create(user: alt_rock_u1, artist: a110)
+
+# #profile: hip-hop fan
+# a21 = add_artistgenre_seed("Kanye West")
+# a22 = add_artistgenre_seed("JAY-Z")
+# a23 = add_artistgenre_seed("Chance The Rapper")
+# a24  = add_artistgenre_seed("MF DOOM")
+# a25  = add_artistgenre_seed("Rihanna")
+# a26  = add_artistgenre_seed("Tupac")
+# a27  = add_artistgenre_seed("Notorious B.I.G")
+# a28  = add_artistgenre_seed("The Roots")
+# a29  = add_artistgenre_seed("Gangstarr")
+# a210 = add_artistgenre_seed("Childish Gambino")
+# hip_hop_u1 = User.create(name:"bill", age: 23, city: "NYC", email: "billemail")
+# UserArtist.create(user: hip_hop_u1, artist: a21)
+# UserArtist.create(user: hip_hop_u1, artist: a22)
+# UserArtist.create(user: hip_hop_u1, artist: a23)
+# UserArtist.create(user: hip_hop_u1, artist: a24)
+# UserArtist.create(user: hip_hop_u1, artist: a25)
+# UserArtist.create(user: hip_hop_u1, artist: a26)
+# UserArtist.create(user: hip_hop_u1, artist: a27)
+# UserArtist.create(user: hip_hop_u1, artist: a28)
+# UserArtist.create(user: hip_hop_u1, artist: a29)
+# UserArtist.create(user: hip_hop_u1, artist: a210)
+
+# #profile: pop fan
+# a31 = add_artistgenre_seed("Kanye West")
+# a32 = add_artistgenre_seed("Rihanna")
+# a33 = add_artistgenre_seed("Tame Impala")
+# a34 = add_artistgenre_seed("Billie Eilish")
+# a35  = add_artistgenre_seed("Justin Bieber")
+# a36 = add_artistgenre_seed("Maroon 5")
+# a37  = add_artistgenre_seed("Ed Sheeran")
+# a38  = add_artistgenre_seed("Michael Jackson")
+# a39 = add_artistgenre_seed("Sean Paul")
+# a310 = add_artistgenre_seed("Eminem")
+# pop_u1 = User.create(name:"cameron", age: 43, city: "NYC", email: "cameronemail")
+# UserArtist.create(user: pop_u1, artist: a31)
+# UserArtist.create(user: pop_u1, artist: a32)
+# UserArtist.create(user: pop_u1, artist: a33)
+# UserArtist.create(user: pop_u1, artist: a34)
+# UserArtist.create(user: pop_u1, artist: a35)
+# UserArtist.create(user: pop_u1, artist: a36)
+# UserArtist.create(user: pop_u1, artist: a37)
+# UserArtist.create(user: pop_u1, artist: a38)
+# UserArtist.create(user: pop_u1, artist: a39)
+# UserArtist.create(user: pop_u1, artist: a310)
+
+# #profile: grunge fan
+# a41 = add_artistgenre_seed("Nirvana")
+# a42 = add_artistgenre_seed("Pearl Jam")
+# a43 = add_artistgenre_seed("Alice in Chains")
+# a44 = add_artistgenre_seed("The Smashing Pumpkins")
+# a45  = add_artistgenre_seed("Pavement")
+# a46 = add_artistgenre_seed("Soundgarden")
+# a47  = add_artistgenre_seed("Guided By Voices")
+# a48  = add_artistgenre_seed("Stone Temple Pilots")
+# a49 = add_artistgenre_seed("Korn")
+# a410 = add_artistgenre_seed("Limp Bizkit")
+# grunge_u1 = User.create(name:"vandad", age: 37, city: "Brooklyn", email: "vandademail")
+# UserArtist.create(user: grunge_u1, artist: a41)
+# UserArtist.create(user: grunge_u1, artist: a42)
+# UserArtist.create(user: grunge_u1, artist: a43)
+# UserArtist.create(user: grunge_u1, artist: a44)
+# UserArtist.create(user: grunge_u1, artist: a45)
+# UserArtist.create(user: grunge_u1, artist: a46)
+# UserArtist.create(user: grunge_u1, artist: a47)
+# UserArtist.create(user: grunge_u1, artist: a48)
+# UserArtist.create(user: grunge_u1, artist: a49)
+# UserArtist.create(user: grunge_u1, artist: a410)
+
+# #profile: electronic fan
+# a51 = add_artistgenre_seed("Aphex Twin")
+# a52 = add_artistgenre_seed("BTS")
+# a53 = add_artistgenre_seed("Skrillex")
+# a54 = add_artistgenre_seed("Tygo")
+# a55  = add_artistgenre_seed("Oneohtrix Point Never")
+# a56 = add_artistgenre_seed("Ryuichi Sakamoto")
+# a57  = add_artistgenre_seed("Yellow Magic Orchestra")
+# a58  = add_artistgenre_seed("Kanye West")
+# a59 = add_artistgenre_seed("MF DOOM")
+# a510 = add_artistgenre_seed("J Dilla")
+# electronic_u1 = User.create(name:"leslie", age: 23, city: "NYC", email: "leslieemail")
+# UserArtist.create(user: electronic_u1, artist: a51)
+# UserArtist.create(user: electronic_u1, artist: a52)
+# UserArtist.create(user: electronic_u1, artist: a53)
+# UserArtist.create(user: electronic_u1, artist: a54)
+# UserArtist.create(user: electronic_u1, artist: a55)
+# UserArtist.create(user: electronic_u1, artist: a56)
+# UserArtist.create(user: electronic_u1, artist: a57)
+# UserArtist.create(user: electronic_u1, artist: a58)
+# UserArtist.create(user: electronic_u1, artist: a59)
+# UserArtist.create(user: electronic_u1, artist: a510)
+
+# #profile: hiphop/pop fans
+# hip_hop_pop_u1 = User.create(name: "chrys", age: 97, city: "NJ", email: "chrysemail")
+# UserArtist.create(user: hip_hop_pop_u1, artist: a21)
+# UserArtist.create(user: hip_hop_pop_u1, artist: a22)
+# UserArtist.create(user: hip_hop_pop_u1, artist: a23)
+# UserArtist.create(user: hip_hop_pop_u1, artist: a24)
+# UserArtist.create(user: hip_hop_pop_u1, artist: a25)
+# UserArtist.create(user: hip_hop_pop_u1, artist: a36)
+# UserArtist.create(user: hip_hop_pop_u1, artist: a37)
+# UserArtist.create(user: hip_hop_pop_u1, artist: a38)
+# UserArtist.create(user: hip_hop_pop_u1, artist: a39)
+# UserArtist.create(user: hip_hop_pop_u1, artist: a310)
+
+# hip_hop_pop_u2 = User.create(name: "jess", age: 92, city: "Connecticut", email: "jessemail")
+# UserArtist.create(user: hip_hop_pop_u2, artist: a31)
+# UserArtist.create(user: hip_hop_pop_u2, artist: a32)
+# UserArtist.create(user: hip_hop_pop_u2, artist: a33)
+# UserArtist.create(user: hip_hop_pop_u2, artist: a34)
+# UserArtist.create(user: hip_hop_pop_u2, artist: a35)
+# UserArtist.create(user: hip_hop_pop_u2, artist: a26)
+# UserArtist.create(user: hip_hop_pop_u2, artist: a27)
+# UserArtist.create(user: hip_hop_pop_u2, artist: a28)
+# UserArtist.create(user: hip_hop_pop_u2, artist: a29)
+# UserArtist.create(user: hip_hop_pop_u2, artist: a210)
 
 
 #artist seeds
